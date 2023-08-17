c2248b8de968f7c645273f07f26a7b6ca3534f1e
Maria Coelho <mariiacoelho25@hotmail.com>
2023-08-17 12:44:01 +0200

    rails new

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,3 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-// app/assets/config/manifest.js
-//= link popper.js
-//= link bootstrap.min.js
